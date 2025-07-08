36fdc45e2bde3e61a9c749045073cdb297f37760
Raquel <cvhrem03@gmail.com>
2025-07-07 18:23:18 -0600

    update

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,39 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\TodosController;
+use App\Http\Controllers\CategoriesController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/todos', function () {
-    return view('todos.index');
-});
 
+Route::get('/todos', TodosController::class . '@index')->name('todos');
+
+Route::post('/todos', TodosController::class . '@store');
+
+Route::delete('/todos/{id}', [TodosController::class , 'destroy'])->name('todos-destroy');
+
+Route::get('/todos/{id}', [TodosController::class , 'show'])->name('todos-edit');
+
+Route::patch('/todos/{id}', [TodosController::class , 'update'])->name('todos-update');
+
+// Categories
+Route::resource('categories', CategoriesController::class);
+
+
+// Products
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
 Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
